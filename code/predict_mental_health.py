001b5b98757f375dc3f65f4804b6f5cee2a51788
Srimanth <109213942+AISpaceXDragon@users.noreply.github.com>
2025-02-07 00:28:26 +0530

    Update predict_mental_health.py

--- code/predict_mental_health.py
+++ code/predict_mental_health.py
@@ -37,7 +37,7 @@ if 'loaded' not in st.session_state:
 st.title("Mental Health Prediction & Explainability")
 
 # Create two columns for input (left) and output (right)
-col1, col2 = st.columns([2, 5])
+col1, col2 = st.columns([1, 5])
 
 # Left Column: User Inputs
 with col1:
